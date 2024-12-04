1c1387dc1887cf7fe109c60091bb97fb9ef4e22b
thon <noth.dwarf@gmail.com>
2024-12-04 17:34:20 +0500

    ProgSkeleton

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
-using namespace std;
+// Функция № 1: Читает строки из текстового файла и сохраняет их в вектор
+void readFromFile(const std::string& filename, std::vector<std::string>& lines) {
+    // Пустая функция
+}
 
-int main()
-{
-    cout << "Hello world!" << endl;
+// Функция № 2: Выводит строки из вектора на экран
+void printLines(const std::vector<std::string>& lines) {
+    // Пустая функция
+}
+
+// Функция № 3: Записывает строки из вектора в файл
+void writeToFile(const std::string& filename, const std::vector<std::string>& lines) {
+    // Пустая функция
+}
+
+int main() {
+    std::string inputFilename = "input.txt";
+    std::string outputFilename = "output.txt";
+    std::vector<std::string> lines;
+
+    // Вызов функций
+    readFromFile(inputFilename, lines);
+    printLines(lines);
+    writeToFile(outputFilename, lines);
+
     return 0;
 }
